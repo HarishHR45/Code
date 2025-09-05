838731552feedb7ee93f33c43e1843a85014f8bb
Harish Raghavan V <125890127+HarishHR45@users.noreply.github.com>
2025-09-05 16:35:29 +0530

    Time: 13 ms (42.22%) | Memory: 45.1 MB (23.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,11 +3,32 @@ class Solution {
       String  str = s.toLowerCase().replaceAll("[^a-z0-9]","");
       int l = 0 , r = str.length()-1;
       while(l < r){
-        if(str.charAt(l) != str.charAt(r)){
+        char a = str.charAt(l);
+        char b = str.charAt(r);
+
+        if(a >= 'A' && a <= 'Z'){
+            a = (char)(a+32);
+        }
+        if(b >= 'A' && b <= 'Z'){
+            b = (char)(b+32);
+        }
+
+        if(!((a >= 'a' && a <= 'z') || (a >= '0' && a <= '9'))){
+            l++;
+            continue;
+        }
+        if(!((b >= 'a' && b <= 'z') || (b >= '0' && b <= '9'))){
+            r--;
+            continue;
+        }
+
+        if(a != b){
             return false;
         }
+
         l++;
         r--;
+
       }
       return true;
     }
