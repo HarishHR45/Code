6fccbcf72ad450b3d19d2554f80a6c17e8531658
Harish Raghavan V <125890127+HarishHR45@users.noreply.github.com>
2025-09-25 20:59:05 +0530

    Time: 1 ms (76.94%) | Memory: 45.2 MB (38.71%) - LeetSync

---
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int earliestTime(int[][] tasks) {
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<tasks.length;i++){
+            if(tasks[i][0] + tasks[i][1] < res){
+                 res = tasks[i][0] + tasks[i][1];
+            }
+        }
+        return res;
+    }
+}
