cd6de6aab72eadf1ae29aef3a8de02cbe0ba5aad
Harish Raghavan V <125890127+HarishHR45@users.noreply.github.com>
2025-09-28 19:43:14 +0530

    Time: 3 ms (58.07%) | Memory: 49.9 MB (96.28%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -1,15 +1,13 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0,count = 0,left=0;
-        while(left < nums.length) {
-            if(nums[left] == 1){
-                left++;
+        int max = 0,count = 0;
+        for(int n : nums){
+            if(n == 1) {
                 count++;
                 max = Math.max(max,count);
             }
             else{
-                count=0;
-                left++;
+                count =0;
             }
         }
         return max;
